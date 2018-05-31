aa7bd7fec255a22261dcc3751a66f249b8808297
dyazincahya <cahyadyazin@yahoo.com>
2018-05-31 17:31:45 +0700

    pre...

---
+++ dist/js/einfachx.js
@@ -0,0 +1,18 @@
+/* einfachX app.js
+    * ================
+    * Main JS application file for einfachX v0.1. This file
+    * should be included in all pages. It controls some layout
+    * options and implements exclusive einfachX plugins.
+    *
+    * @Author  K4ng
+    * @Support <https://www.einfachx.github.io>
+    * @Email   <dyazincahya@gmail.com>
+    * @version 0.1
+    * @repository git://github.com/dyazincahya/einfachX.git
+    * @license MIT <http://opensource.org/licenses/MIT>
+ */
+
+
+if (typeof jQuery === 'undefined') {
+    throw new Error('einfachX requires jQuery')
+}
